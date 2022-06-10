7761c13cdd55b1e05c51ea3466f507bddd0ee541
Andrea <a.carreocoppo2@gmail.com>
2022-06-10 02:00:40 -0400

    Actualizacion carrito

--- js/script.js
+++ js/script.js
@@ -2,71 +2,56 @@ $(function () {
     $('[data-toggle="tooltip"]').tooltip()
   })
 
-function datos() {
-    //Solicitud de datos
-    let nameClient = prompt("Ingresa tu nombre");
-    let lastNameClient = prompt("Ingresa tu apellido");
-
-    //Imprimiendo datos
-    document.getElementById("name-client").innerHTML = nameClient;
-
-
-    let lastName = document.getElementById("last-name-client");
-    lastName.innerHTML = lastNameClient;
-}
-
-let i = 0;
-
-function incremento() {
-    ++i;
-    document.querySelector(".contador").innerHTML = i;
-    console.log(i)
-    if (i >= 20) {
-        document.querySelector(".contador").style.color = "green";
-    } else {
-        document.querySelector(".contador").style.color = "black";
-    }
-
-}
-function decremento() {
-    --i;
-    document.querySelector(".contador").innerHTML = i;
-    console.log(i)
-    if (i < 0) {
-        document.querySelector(".contador").style.color = "red";
-    } else {
-        document.querySelector(".contador").style.color = "black";
-    }
-}
 
 let productos = [
     {
         id: 1,
-        imagen: "./img/img-1.jpeg",
-        producto: "Girasol",
-        precio: 300
+        imagen: "img/thelastofus2.webp",
+        producto: "The Last Of Us II",
+        precio: 59990
     },
     {
         id: 2,
-        imagen: "./img/img-2.jpeg",
-        producto: "Margarita",
-        precio: 200
+        imagen: "./img/minecraft.webp",
+        producto: "Minecraft Starter Refresh",
+        precio: 32000
     },
     {
         id: 3,
-        imagen: "./img/img-3.jpeg",
-        producto: "Tulipan",
-        precio: 100
+        imagen: "./img/horizon.webp",
+        producto: "Horizon Zero Dawn Complete Edition",
+        precio: 19990
     },
     {
         id: 4,
-        imagen: "./img/img-4.jpeg",
-        producto: "Gladiolo",
-        precio: 400
+        imagen: "./img/gt.webp",
+        producto: "Gran Turismo Sport",
+        precio: 49990
+    },
+    {
+        id: 5,
+        imagen: "./img/codv.webp",
+        producto: "Call Of Duty Vanguard",
+        precio: 64990
+    
+    },
+    {
+        id: 6,
+        imagen: "./img/ghost.webp",
+        producto: "Ghost Of Tsushima Directors Cut",
+        precio: 54990
+    
+    },
+    {
+        id: 7,
+        imagen: "./img/spider.webp",
+        producto: "Spiderman Miles Morales Marvel",
+        precio: 49990
     }
 ]
 
 let inputBuscador;
+
 
 let prueba = document.getElementById("prueba"); //se guarda el lugar donde se imprimira
 
@@ -75,17 +60,19 @@ function principal() {
     prueba.innerHTML = ""; //limpiar html
     productos.map(function (item) { //se recorre el array de objetos con .map
         prueba.innerHTML += ` 
-        <div  class="col-12 col-sm-4 col-md-3 p-3">
-        <div class="card text-dark ">                       
-            <h3 class="card__title p-3">${item.producto}</h3>
-            <img src=${item.imagen} class="card__img rounded-circle px-5" alt="flores">
-            <div class="card-body">
-                <p class="card__text">Precio: $${item.precio}</p>
-                <button onclick="addToCart(${item.id})" type="button" class="btn btn-sm btn-2" data-toggle="tooltip" data-placement="top"
-                title="Añadir al carrito">Comprar</button>
-            </div>
+        <article class="card">
+        <picture class="text-center">
+            <img src=${item.imagen} class="" alt="flores">
+        </picture>
+        <div class="card-content">
+          <h3 class="card__title">${item.producto}</h3>
+       </div>
+        <div class="card__footer d-flex align-items-end align-items-lg-center justify-content-lg-between  flex-column flex-lg-row"> 
+            <p class="m-0">$${item.precio}</p>
+            <button onclick="addToCart(${item.id})" class="btn btn-sm btn-2" data-toggle="tooltip" data-placement="top"
+            title="Añadir al carrito">Comprar</button>
         </div>
-        </div>
+      </article>
         `
     })
 }
@@ -103,7 +90,7 @@ function buscar2() {
         return transformarNombre.includes(inputBuscador.toLowerCase()) //se compara el objeto que se busca con el valor del input
     })
 
-    console.log(arrayFiltrado);
+    // console.log(arrayFiltrado);
     if (arrayFiltrado.length === 0) { //si el largo del array es 0 entonces imprime mensaje
         prueba.innerHTML += `  
         <h3>Producto no encontrado</h3>
@@ -112,21 +99,28 @@ function buscar2() {
     } else { //si el array viene con elementos entonces los pinta en el html
         arrayFiltrado.map(function (item) {
             prueba.innerHTML += `
-            <div  class="col-12 col-sm-4 col-md-3 p-3">
-            <div class="card text-dark ">                       
-                <h3 class="card__title p-3">${item.producto}</h3>
-                <img src=${item.imagen} class="card__img rounded-circle px-5" alt="flores">
-                <div class="card-body">
-                    <p class="card__text">Precio: $${item.precio}</p>
-                    <button onclick="addToCart(${item.id})" type="button" class="btn btn-sm btn-2" data-toggle="tooltip" data-placement="top"
-                    title="Añadir al carrito">Comprar</button>
-                </div>
+            <article class="card">
+            <picture class="text-center">
+                <img src=${item.imagen} class="" alt="flores">
+            </picture>
+            <div class="card-content">
+              <h3 class="card__title">${item.producto}</h3>
+           </div>
+            <div class="card__footer "> 
+                <p class="m-0">$${item.precio}</p>
+                <button onclick="addToCart(${item.id})" class="btn btn-sm btn-2" data-toggle="tooltip" data-placement="top"
+                title="Añadir al carrito">Comprar</button>
             </div>
-            </div>
+          </article>
+
+
         `
         })
     }
 }
+
+
+
 
 //funcion ejecuta al cargar la pagina
 function ejecutar() {
@@ -141,79 +135,96 @@ ejecutar();
 
 let cartNuevo = [];
 
+
+let contadorCarrito = 0;
+
+let ubicacionContadorCarrito = document.getElementById("contadorCarrito");
+
+function contador () {
+
+}
+
 function addToCart(idProduct) {
     // console.log(idProduct)
     let producto = productos.filter(item => item.id === idProduct)
-    console.log(producto)
-    console.log(producto[0])
+    // console.log(producto)
+    // console.log(producto[0])
     let boolean = cartNuevo.some(item => item.id === idProduct)
-    console.log(boolean)
+    // console.log(boolean)
     if (boolean) {
+        contadorCarrito= contadorCarrito + 1;
+        console.log(contadorCarrito)
+
         producto[0].count = producto[0].count + 1;
         producto[0].subTotal = producto[0].subTotal + producto[0].precio;
         mostrarCarritoEnHtml()
     } else {
+        contadorCarrito= contadorCarrito + 1;
+        console.log(contadorCarrito)
+
         producto[0].count = 1;
         producto[0].subTotal = producto[0].precio;
         cartNuevo.push(producto[0])
         mostrarCarritoEnHtml()
     }
-    console.log("nuevo arreglo", cartNuevo);
+   
+    // console.log("nuevo arreglo", cartNuevo);
 }
+
+
 
 
 
 let total = 0;
 function mostrarCarritoEnHtml() {
     document.querySelector(".cart").innerHTML = "";
-    cartNuevo.map(function (item) {
+
+    ubicacionContadorCarrito.innerHTML = "";
+    ubicacionContadorCarrito.innerHTML = contadorCarrito;
+
+ 
+    cartNuevo.map(function (item,index) {
+
+
+      
         total = item.subTotal + total;
+      
         document.querySelector(".cart").innerHTML += `
-        
-            <div class="cart-item row px-md-5 ">
-                <div class="col-1 mb-1 d-none d-sm-block ">
-                    <img class="cart-item__img" src="${item.imagen}" alt="img">
-                </div>
-                <div class="col-3  mb-1 ">
-                    <p class="col-2 p-0 mb-0 cart-item__name">${item.producto}</p>
-                </div>
-                <div class="col-2 mb-1">
-                    <p class="cart-item__price mb-0">$${item.precio}</p>
-                </div>
-                <div class="col-2  mb-1">
-                    <p class="cart-item__counts mb-0">${item.count}</p>
-                </div>
-                <div class="col-2 mb-1">
-                    <p class="cart-item__price mb-0">$${item.subTotal}</p>
-                </div>
-                <div class="col-2 d-flex justify-content-end">
-                    <button onclick="eliminarProducto(${item.id})" class=" my-1 text-right cart-item__count btn btn-2">X</button>
-                </div>
-            </div>
-        
+                <tr class="pb-2">
+                <td scope="row">${item.producto} </td>
+                <td>$${item.precio}</td>
+                <td class="text-center">${item.count}</td>
+                <td>$${item.subTotal}</td>
+                <td> <button onclick="eliminarProducto(${item.id})" class="ml-1 text-right btn p-1"><img src="../img/icons/trash.svg"></button> <td>
+                </tr>
      `
 
         document.querySelector(".valor").innerHTML = `
-        <div class="d-flex justify-content-end">
-            <button onclick="vaciarCarrito()" class=" py-0 mx-4  d-inline  btn btn-2">Vaciar carrito</button>
-            <h3 class=" my-3 mr-md-5 d-inline ">Total: ${total}</h3>
+        <div class="valortotal d-flex justify-content-between align-items-center">
+            <p onclick="vaciarCarrito()" class=" d-inline  btnvaciar m-0 p-1 ">Vaciar carrito</p>
+            <p class="m-0 d-inline ">Total: $${total}</p>
         </div>
     `
         
-        console.log(total)
+        // console.log(total)
     });
     total = 0
 }
 
-console.log(total)
+
+// console.log(total)
 
 function eliminarProducto(itemProducto) {
     cartNuevo = cartNuevo.filter(item => item.id != itemProducto)
+    ubicacionContadorCarrito.innerHTML = "";
+    contadorCarrito--
     document.querySelector(".valor").innerHTML = "";
     mostrarCarritoEnHtml()
 }
 
 function vaciarCarrito() {
+    ubicacionContadorCarrito.innerHTML = "";
+    contadorCarrito--
     cartNuevo = [];
     document.querySelector(".valor").innerHTML = "";
     document.querySelector(".cart").innerHTML = "";
